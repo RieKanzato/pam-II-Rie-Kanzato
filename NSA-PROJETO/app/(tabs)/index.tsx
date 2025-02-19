d5fb6a5945bf92f4d5abbcf9fb0f5ff33085bffc
Rie Kanzato <mrie.kanzato@gmail.com>
2025-02-18 22:42:40 -0300

    eu vou chorar

--- NSA-PROJETO/app/(tabs)/index.tsx
+++ NSA-PROJETO/app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Image } from "react-native";
 
 export default function Index() {
-  return (
-      <View style={styles.container}>
-      <View style={styles.navContainer}>
+  const horarios = require('../../assets/images/clock.png');
+  const aulas = require('../../assets/images/calendar.png');
+  const boletim = require('../../assets/images/document .png');
 
-      </View>
+  return (
+      <View style={styles.container}> 
+      <View style={[styles.navContainer,{flexDirection: 'row'},]}>
+            <Image source={ horarios } style={[styles.icon1, {backgroundColor: 'white'},]} />
+            <Image source={ aulas } style={[styles.icon2, {backgroundColor: 'white'},]} />
+            <Image source={ boletim } style={[styles.icon3, {backgroundColor: 'white'},]} />
+        </View>  
+        <View style={styles.mensagens}>
+             <Text style={[styles.msg,{fontSize: 20},]}> Mensagens </Text> 
+        </View>
+        <View style={styles.pendencias}>
+        <Text style={[styles.msg, {fontSize: 20},]}> Pendencias </Text> 
+        </View>
       </View>
   );
 }
@@ -17,18 +29,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff'
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color:'#fff',
-  },
   navContainer: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    borderBottomColor: 'white',
-    
+    top: 30,
+    flex: 1,
+  },
+  icon1: {
+    right: 50,
+    height: 90,
+    width: 90,
+    borderWidth: 5,
+    borderRadius: 100,
+  },
+  icon2: {
+    height: 90,
+    width: 90,
+    borderWidth: 5,
+    borderRadius: 100,
+  },
+  icon3: {  
+    left: 50,
+    height: 90,
+    width: 90,
+    borderWidth: 5,
+    borderRadius: 100, 
+  },
+  mensagens: {
+    backgroundColor: '#FAFAFA',
+    height: 150,
+    width: 350,
+    bottom: 130,
+    borderRadius: 10,
+  },
+  pendencias: {
+    backgroundColor: '#FAFAFA',
+    height: 150,
+    width: 350,
+    bottom: 60,
+    borderRadius: 10
+  },
+  msg:{
+    fontWeight: 'bold',
   }
 })
--- NSA-PROJETO/app/(tabs)/perfil.tsx
+++ NSA-PROJETO/app/(tabs)/perfil.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
+
 export default function PerfilScreen() {
+    const perfilPic = require('../../assets/images/iconPfp.png');
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
     const [items, setItems] = useState([
@@ -12,28 +14,32 @@ export default function PerfilScreen() {
   return (
     
     <View style={styles.container}>
-     <View style={styles.perfil}>
-      <Text style={styles.username}>{'\n'}User Sobrenome{'\n'}</Text>
-      <Text style={styles.info}>
-      {'\n'}RM:{'\n'}
-      {'\n'}RA SED:{'\n'}
-      {'\n'}Mod/Serie Atual:{'\n'}
-      {'\n'}Turma:{'\n'}
-      {'\n'}Grupo do Componente:{'\n'}
-      {'\n'}Sit. da Matricula:{'\n'}
-      </Text>
-      <Text style={styles.hab}>{'\n'}Habilitação{'\n'}</Text>
-      <View style= {styles.picker}>
-          <DropDownPicker
-          open={open}
-          value={value}
-          items={items}
-          setOpen={setOpen}
-          setValue={setValue}
-          setItems={setItems}
-        />
+      <View style={styles.perfil}> 
+        <View style={styles.iconContainer}>
+          <Image source={ perfilPic } style={styles.icon} />
+        </View>
+          <Text style={styles.username}>{'\n'}User Sobrenome{'\n'}</Text>
+          <Text style={styles.info}>
+          {'\n'}RM:{'\n'}
+          RA SED:{'\n'}
+          Mod/Serie Atual:{'\n'}
+          Turma:{'\n'}
+          Grupo do Componente:{'\n'}
+          Sit. da Matricula:{'\n'}
+          {'\n'}
+          </Text>
+          <Text style={styles.hab}>{'\n'}Habilitação{'\n'}</Text>
+          <View style= {styles.picker}>
+            <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+          />
+        </View>
       </View>
-     </View>
     </View>
   );
 }
@@ -45,12 +51,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  iconContainer:{
+    alignContent: 'center',
+  },
+  icon:{
+    top: 20,
+    height: 150,
+    width: 150,
+    borderRadius: 100,
+    borderWidth: 3,
+    borderColor: '#3C3C3C',
+  },
   perfil: {
     backgroundColor: '#FAFAFA',
     alignItems: 'center',
-    top: 35,
+    flex: 1,
     width: '90%',
-    height: '85%',
+    height: '95%',
   },
   username: {
     color: 'black',
