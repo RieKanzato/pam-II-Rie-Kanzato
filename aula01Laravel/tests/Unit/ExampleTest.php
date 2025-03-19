1897f6e15d6b35eb86d658dab5618e3dfd62652a
Rie Kanzato <mrie.kanzato@gmail.com>
2025-03-19 11:57:26 -0300

    aula de laravel

--- aula01Laravel/tests/Unit/ExampleTest.php
+++ aula01Laravel/tests/Unit/ExampleTest.php
@@ -6,11 +6,24 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function test_same(): void
+    {
+        $this->assertSame('20', 20);
+    }
+
+    public function test_equals(): void
+    {
+        $this->assertEquals(1, 0);
+    }
+    
+    public function teste_assert_false(): void
+    {
+        $this->assertFalse(true);
     }
 }
